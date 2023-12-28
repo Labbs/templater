6eff92ce9cd214e93f15511b58bd11f608ea133d
Mouton Jeremy <moutonjeremy@labbs.fr>
2023-12-28 16:27:11 +0100

    split generic and specific config

--- command/create/config/config.go
+++ command/create/config/config.go
@@ -3,7 +3,7 @@ package config
 var AppConfig Config = Config{}
 
 type Config struct {
-	Version    string
-	Debug      bool
-	PrettyLogs bool
+	ValuesFile    string
+	TemplateFiles string
+	OutputFiles   string
 }
